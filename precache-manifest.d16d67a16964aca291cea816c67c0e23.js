91b194cf257d8ab37b94daa51b92fdcaba2d8cf1
smfils1 <smfils1@gmail.com>
2020-04-26 23:35:46 -0400

    Updates

---
+++ precache-manifest.d16d67a16964aca291cea816c67c0e23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5a247a100d0ceb9326f305643c8a59",
+    "url": "/SoS-web/index.html"
+  },
+  {
+    "revision": "f2843610a93a86806c9a",
+    "url": "/SoS-web/static/css/main.8b92722e.chunk.css"
+  },
+  {
+    "revision": "94316a41c847b3c84dbf",
+    "url": "/SoS-web/static/js/2.3ff94438.chunk.js"
+  },
+  {
+    "revision": "f2843610a93a86806c9a",
+    "url": "/SoS-web/static/js/main.5b3b8737.chunk.js"
+  },
+  {
+    "revision": "f1df144c34fa26d78d7f",
+    "url": "/SoS-web/static/js/runtime~main.8ddf747c.js"
+  },
+  {
+    "revision": "ac531091f252a529077b05cf39c3bfc0",
+    "url": "/SoS-web/static/media/hero_image.ac531091.jpg"
+  }
+]);

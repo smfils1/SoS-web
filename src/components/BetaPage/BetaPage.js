7b108330a542a675b970d414bd48e7be0ac8e34c
smfils1 <smfils1@gmail.com>
2020-04-30 00:20:50 +0000

    Added game responsiveness

--- src/components/BetaPage/BetaPage.js
+++ src/components/BetaPage/BetaPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Unity, { UnityContent } from "react-unity-webgl";
+import "./BetaPage.css"
+
 const unityContent = new UnityContent(
   "build/Build/build.json",
   "build/Build/UnityLoader.js"
 );
 
 const BetaPage = () => {
-  return <Unity unityContent={unityContent} />;
+  return <div className="w-100 bg-secondary"><div className="unity"><Unity unityContent={unityContent} /></div></div>;
 };
 
 export default BetaPage;
